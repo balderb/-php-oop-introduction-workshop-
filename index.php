4736738a9e77465275f897859e5c28d33e7f4f78
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-16 15:19:22 +0200

    declaring properties + creating foresee construct

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+class beverage {
+
+    // declare properties
+
+    private $color(string);
+    private $price(float);
+    private $temperature (string);
+
+// + foresee a construct
+
+}
