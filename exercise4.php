ccb07d38619270fb7426c4a3815c0f64b221b7c2
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-09-17 11:55:40 +0200

    adding exersice 4

---
+++ exercise4.php
@@ -0,0 +1,45 @@
+<?php
+
+// Oefening 4 (first attempt)
+
+class Beer extends Beverage {
+
+// add two more variables to the extended class Beer
+public string $name;
+public float $alcoholpercentage;
+
+// create a construct with the two extended (and public) variables (not sure about semicolons at the end)
+public function __construct(
+string $color,
+float $price,
+string $temperature = "cold",
+float $alcoholpercentage,
+string $name
+);
+
+// Foresee a construct with previous properties
+//// + sets values for name and alcoholpercentage (with the '=' symbol).
+{
+parent::__construct(
+$color,
+$price,
+$temperature
+);
+
+$this->name = $name;
+$this->alcoholpercentage = $alcoholpercentage;
+
+// Make getAlcoholpercentage (public function)
+public function getAlcoholpercentage(): float
+{
+return $this->alcoholpercentage;
+}
+
+$duvel = new Beer('Duvel', 8.5, 'blond', 3.5);
+echo $duvel->getAlcoholpercentage() . "<br>";
+echo $duvel->alcoholpercentage . "<br>";
+echo $duvel->color . "<br>";
+echo $duvel->getInfo() . "<br>";
+
+$cola = new Beverage('black', 2);
+echo $cola->getAlcoholpercentage();
